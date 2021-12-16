c17909572793100c528748d32aff01104cc4813c
Sepehr <sepehrafla@gmail.com>
2021-12-16 11:19:20 -0500

    item viewmodel

--- demoProject/demoProject/ViewModel/ItemViewModel.swift
+++ demoProject/demoProject/ViewModel/ItemViewModel.swift
@@ -9,9 +9,23 @@ import Foundation
 import RxSwift
 
 class ItemViewModel {
+    let disposeBag = DisposeBag()
     weak var coordinator: ItemCoordinator?
+    private var cartButton = UIBarButtonItem()
     func login() {
         coordinator?.navigateToCartScreen()
+    }
+    func goBack() {
+        ShoppingCart.sharedCart.cars.accept([])
+        coordinator?.navigateToCartScreen()
+    }
+    func setupCartObserver() {
+      ShoppingCart.sharedCart.cars.asObservable()
+        .subscribe(onNext: { [unowned self] setOfCars in
+            self.cartButton.title = "\u{1F6D2} (\(setOfCars.count))"
+            print(setOfCars.count)
+        })
+        .disposed(by: disposeBag)
     }
 }
 protocol ItemCoordinator: AnyObject {
